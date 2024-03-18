ad1ef9fa1eefe0b923286938214285bdab68c7ab
0rsen0 <arsens733@gmail.com>
2024-03-18 21:27:36 +0100

    dgfdgsf

--- src/Task2.java
+++ src/Task2.java
@@ -2,10 +2,13 @@ public class Task2 {
     public static void main(String[] args) {
 
 
+
 //        git init
 //        git remote add origin https://github.com/ArsenSkrynnyk/a.git
 //        git branch -M main
-
+//        git add .
+//        git commit -m "first commit"
+//        git push -u origin main   (send to gitHub)
 
     }
 }
